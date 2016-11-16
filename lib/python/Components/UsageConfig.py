e501b8b1e698bcbc036d000cc98e4226f2a9ee9a
herpoi <herpoi2006@gmail.com>
2016-11-16 14:38:18 +0100

    GOS Update

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -92,7 +92,7 @@ def InitUsageConfig():
 	for i in (3600, 7200, 14400):
 		h = i / 3600
 		choicelist.append((str(i), ngettext("%d hour", "%d hours", h) % h))
-	config.usage.hdd_standby = ConfigSelection(default = "300", choices = [("0", _("No standby"))] + choicelist)
+	config.usage.hdd_standby = ConfigSelection(default = "-1", choices = [("-1", _("Controlled outside")), ("0", _("No standby"))] + choicelist)
 	config.usage.output_12V = ConfigSelection(default = "do not change", choices = [
 		("do not change", _("Do not change")), ("off", _("Off")), ("on", _("On")) ])
 
